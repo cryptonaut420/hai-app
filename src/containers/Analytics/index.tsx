acfbf42716dbfbfc5188f04ad40aae4d702f50e8
cryptonaut <nrathman@ironcladtech.ca>
2025-04-14 00:00:46 -0700

    cleanup a bit more for now

--- src/containers/Analytics/index.tsx
+++ src/containers/Analytics/index.tsx
@@ -1,11 +1,9 @@
-import { CollateralList } from './CollateralList'
 import { Numbers } from './Numbers'
 
 export function Analytics() {
     return (
         <>
             <Numbers />
-            <CollateralList />
         </>
     )
 }
